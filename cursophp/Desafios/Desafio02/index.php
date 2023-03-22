cc78cbe4fc2decbf515c7a930041a644d3ab6b08
PedroHenriqueFeitosa <pedro291201@gmail.com>
2023-03-22 08:51:44 -0300

    Update index.php

--- cursophp/Desafios/Desafio02/index.php
+++ cursophp/Desafios/Desafio02/index.php
@@ -5,18 +5,20 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Desafio 002</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style.css">;
 </head>
 <body>
-    <main>
+    <header>
         <h1>Trabalhando com numeros aleatorios </h1>
+</header>
+<main>
         <?php 
         $min=0;
         $max=100;
         $num=mt_rand($min,$max);
         echo "<p>Gerando um numero aleatorio entre $min e $max... <br> O Valor Gerado foi <storng>$num</strong></p>"
         ?>
-        <button>&x1F504;De Novo </button>
+        <button onclick="javasrcipt:document.location.reload()">"&x1F504";De Novo </button>
     </main>
 </body>
 </html>
