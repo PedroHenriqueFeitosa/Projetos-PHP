848e6ef9a7217ffa3ac7be2387cd5c808a9ae52d
PedroHenriqueFeitosa <pedro291201@gmail.com>
2023-03-22 08:43:10 -0300

    New Charges

---
+++ cursophp/Desafios/Desafio02/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt_br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desafio 002</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <main>
+        <h1>Trabalhando com numeros aleatorios </h1>
+        <?php 
+        $min=0;
+        $max=100;
+        $num=mt_rand($min,$max);
+        echo "<p>Gerando um numero aleatorio entre $min e $max... <br> O Valor Gerado foi <storng>$num</strong></p>"
+        ?>
+        <button>&x1F504;De Novo </button>
+    </main>
+</body>
+</html>
