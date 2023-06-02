d15aff948b04626996a91ec283a5bacec08d6ad5
Pedro Henrique Feitosa De Jesus <88751677+PedroHenriqueFeitosa@users.noreply.github.com>
2023-06-02 11:45:10 -0300

    Calculadora de Tempo

---
+++ cursophp/Desafios/Desafio12/index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desafio De PHP</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php 
+    $total=$_REQUEST ['seg'] ?? 0;
+    ?>
+    <main>
+        <h1>Calculadora de Tempo</h1>
+        <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
+        <label for="seg">Qual é o total de segundos?</label>
+        <input type="number" name="seg" id="seg" min="0" step="1" required value="<?=$total?>"> 
+        <input type="submit" value="Calcular">
+        </form>
+    </main>
+    <?php 
+    $sobra=$total;
+    //Total de Semanas
+    $semana=(int)($sobra/604_800);
+    $sobra=$sobra % 604_800;
+    //Total de Dias 
+    $dia=(int)($sobra/86_400);
+    $sobra=$sobra % 86_400;
+    //Total de Horas 
+    $hora=(int)($sobra/3_600);
+    $sobra=$sobra % 3_6000;
+    //Total de Minutos
+    $minuto=(int)($sobra/60);
+    $sobra= $sobra % 60;
+    // Total de Segundos 
+    $segundo=$sobra;
+    
+    ?>
+    <section>
+        <h2>Totalizando Tudo  </h2>
+        <p>Analisando o valor que voce digitou, <strong><?=$total?>segundos</strong> a um total de:</p>
+        <ul>
+        <li><?=$semana?> semanas</li>
+        <li><?=$dia?> dias</li>
+        <li><?=$hora?> horas </li>
+        <li><?=$minuto?> minutos</li>
+        <li><?=$segundo?> segundos </li>
+        </ul>
+    </section>
+</body>
+</html>
